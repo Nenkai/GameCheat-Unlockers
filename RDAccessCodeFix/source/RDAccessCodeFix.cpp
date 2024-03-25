ded6b7aed9a5f3f5dde3e41aa2f201963dc0fbcb
Silent <zdanio95@gmail.com>
2024-03-25 20:32:09 +0100

    Properly set up fixes for TOCA Race Driver 3

--- RDAccessCodeFix/source/RDAccessCodeFix.cpp
+++ RDAccessCodeFix/source/RDAccessCodeFix.cpp
@@ -4,6 +4,12 @@
 #include "Utils/ScopedUnprotect.hpp"
 
 #include <cstdint>
+#include <filesystem>
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+#include <wil/win32_helpers.h>
 
 LSTATUS WINAPI RegCreateKeyEx_LicenseKey(HKEY /*hKey*/, LPCSTR /*lpSubKey*/, DWORD /*Reserved*/, LPSTR /*lpClass*/, DWORD /*dwOptions*/, REGSAM /*samDesired*/,
 				const LPSECURITY_ATTRIBUTES /*lpSecurityAttributes*/, PHKEY phkResult, LPDWORD /*lpdwDisposition*/)
@@ -11,6 +17,29 @@ LSTATUS WINAPI RegCreateKeyEx_LicenseKey(HKEY /*hKey*/, LPCSTR /*lpSubKey*/, DWO
 	return RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", 0, KEY_QUERY_VALUE|KEY_WOW64_64KEY, phkResult);
 }
 static auto* const pRegCreateKeyEx_LicenseKey = &RegCreateKeyEx_LicenseKey;
+
+static int GenerateReferenceCode_Stub()
+{
+	return -1;
+}
+
+static std::wstring GetPathToIni()
+{
+	std::wstring result{L".\\" rsc_Name ".ini"};
+
+	wil::unique_cotaskmem_string pathToAsi;
+	if (SUCCEEDED(wil::GetModuleFileNameW(wil::GetModuleInstanceHandle(), pathToAsi)))
+	{
+		try
+		{
+			result = std::filesystem::path(pathToAsi.get()).replace_extension(L"ini").wstring();
+		}
+		catch (const std::filesystem::filesystem_error&)
+		{
+		}
+	}
+	return result;
+}
 
 void OnInitializeHook()
 {
@@ -21,14 +50,8 @@ void OnInitializeHook()
 	using namespace Memory;
 	using namespace hook::txn;
 
-	// Allow plaintext cheats for every cheat code
-	try
-	{
-		auto cheat_encrypted_only = get_pattern("33 C0 83 F9 0C 73 14", 2);
-		Patch<uint32_t>(cheat_encrypted_only, 0xC3); // put retn after xor eax, eax
-	}
-	TXN_CATCH();
-	
+	const std::wstring pathToIni = GetPathToIni();
+
 	// Query for a correct key from a 64-bit registry view
 	try
 	{
@@ -41,6 +64,28 @@ void OnInitializeHook()
 		{
 			Patch(addr, &pRegCreateKeyEx_LicenseKey);
 		}
+	}
+	TXN_CATCH();
+
+	
+	// Restore rand()-based access code generation, as many people
+	// nowadays share the same ProductId after upgrading to Win10/11
+	try
+	{
+		auto generate_reference_code = get_pattern("56 89 84 24 ? ? ? ? 8D 44 24 0C", -11);
+		auto dont_reset_access_code = get_pattern("74 13 33 C9 89 0D");
+
+		InjectHook(generate_reference_code, GenerateReferenceCode_Stub, HookType::Jump);
+		Patch<uint8_t>(dont_reset_access_code, 0xEB);
+	}
+	TXN_CATCH();
+
+
+	// Allow plaintext cheats for every cheat code
+	if (GetPrivateProfileIntW(L"RDAccessCodeFix", L"PlainCheats", 0, pathToIni.c_str()) != 0) try
+	{
+		auto cheat_encrypted_only = get_pattern("33 C0 83 F9 0C 73 14", 2);
+		Patch<uint32_t>(cheat_encrypted_only, 0xC3); // put retn after xor eax, eax
 	}
 	TXN_CATCH();
 }
